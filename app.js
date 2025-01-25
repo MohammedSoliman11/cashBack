3a81f3756ac671f8a32a0a5d5072e9e8d88626e3
MohammedSoliman11 <mohammedsoliman404@gmail.com>
2025-01-25 05:14:37 +0200

    allow CORS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Importing required modules
 const express = require('express'); // Express.js for building the web application
 const morgan = require('morgan'); // Morgan for HTTP request logging
-// const cors = require('cors'); // Cors for handling Cross-Origin Resource Sharing
+const cors = require('cors'); // Cors for handling Cross-Origin Resource Sharing
 const cookieParser = require('cookie-parser');
 
 
@@ -12,16 +12,20 @@ const userRouter = require('./routes/usersRouter');
 const authRouter = require('./routes/authRouter');
 
 const app = express(); // Creating an Express application instance
+app.use(cors({
+  origin: "http://localhost:5173", // React app's origin
+    credentials: true, // Allow cookies and credentials
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+}));
 app.use(cookieParser());
-
-// const corsOptions = {
-//   origin: 'http://localhost:5173',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
 
 // Middleware setup
 app.use(express.json()); // Parsing JSON request bodies
-// app.use(cors("*")); // Enabling Cross-Origin Resource Sharing for all routes
+
+// Enable CORS with the desired configuration
+
+
 app.use(morgan('dev')); // Using 'dev' format for HTTP request logging
 app.use('/api/v1/auth', authRouter); // Mounting the auth routers
 app.use('/api/v1/users', userRouter); // Mounting the user router at the specified path
