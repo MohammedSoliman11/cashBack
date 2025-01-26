da82570ac36737ea345c8088d7e2941eecba8692
MohammedSoliman11 <mohammedsoliman404@gmail.com>
2025-01-26 19:49:59 +0200

    insert phone number in register

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,6 +22,7 @@ exports.register = async (req, res) => {
     const userData = {
       name: req.body.name,
       email: req.body.email,
+      phone: req.body.phone,
       password: req.body.password,
       passwordConfirm: req.body.passwordConfirm,
     };
